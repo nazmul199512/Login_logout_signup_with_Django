46f2c6a32a794cd81d7853026ed5337f277a955d
nazmul199512 <nazmul199512@gmail.com>
2019-10-21 00:20:52 +0600

    initialize

---
+++ login_signup/Accounts/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth import authenticate, get_user_model
+
+
+User = get_user_model()
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self, *args, **kwargs):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise forms.ValidationError('This user does not exist')
+            if not user.check_password(password):
+                raise forms.ValidationError('Incorrect password')
+            if not user.is_active:
+                raise forms.ValidationError('This user is not active')
+        return super(UserLoginForm, self).clean(*args, **kwargs)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    email = forms.EmailField(label='Email address')
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password',
+            'confirm_password',
+        ]
+
+
+"""
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError("password must match !")
+        return password
+
+"""
